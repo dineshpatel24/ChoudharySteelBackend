0068ce89938bb0060e64ab8f65d88e9b26f1e13b
dineshpatel24 <dineshnpatel24@gmail.com>
2025-10-13 19:37:37 +0530

    .

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import UserRouter from "./routes/userRoutes.js";
 import ProductRouter from "./routes/productRoutes.js";
 import { protect } from "./middleware/authMiddleware.js";
-dotenv.config();
+
 connectDB();
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://your-frontend-domain.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
 app.get("/", (req, res) => res.send("Choudhary Steel API running"));
 
 app.use("/api/users", UserRouter);
